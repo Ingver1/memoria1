377de61bc9b43e1a21667b9f135178f41a839d79
Evgeny <rage19671999@gmail.com>
2025-07-09 00:47:59 +0300

    Create __init__.py

---
+++ pydantic_settings/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from typing import Any
+
+from pydantic import BaseSettings as _BaseSettings
+
+__all__ = ["BaseSettings"]
+
+class BaseSettings(_BaseSettings):
+    pass
