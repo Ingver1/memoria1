528632374de9249366e06f73302cc081b04b5e47
Evgeny <rage19671999@gmail.com>
2025-07-10 11:41:07 +0300

    Update fastapi.py

--- opentelemetry/instrumentation/fastapi.py
+++ opentelemetry/instrumentation/fastapi.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
+from typing import Any
+
+
 class FastAPIInstrumentor:
     @staticmethod
-    def instrument_app(app):
+    def instrument_app(app: Any) -> None:
         return None
