0c01a718e3c19126a9f920011da4406cba04cd7a
Evgeny <rage19671999@gmail.com>
2025-07-11 23:49:05 +0300

    Update pytest_asyncio.py

--- pytest_asyncio.py
+++ pytest_asyncio.py
@@ -8,15 +8,17 @@ def pytest_configure(config):
     config.addinivalue_line("markers", "asyncio: mark async test")
 
 
+LOOP = asyncio.new_event_loop()
+
 @pytest.hookimpl(tryfirst=True)
+
+
 def pytest_pyfunc_call(pyfuncitem):
     testfunc = pyfuncitem.obj
     if inspect.iscoroutinefunction(testfunc):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+        asyncio.set_event_loop(LOOP)
         kwargs = {name: pyfuncitem.funcargs[name] for name in pyfuncitem._fixtureinfo.argnames}
-        loop.run_until_complete(testfunc(**kwargs))
-        loop.close()
+        LOOP.run_until_complete(testfunc(**kwargs))
         asyncio.set_event_loop(None)
         return True
 
@@ -25,27 +27,22 @@ def pytest_pyfunc_call(pyfuncitem):
 def pytest_fixture_setup(fixturedef, request):
     func = fixturedef.func
     if inspect.iscoroutinefunction(func) or inspect.isasyncgenfunction(func):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+        asyncio.set_event_loop(LOOP)
         params = {name: request.getfixturevalue(name) for name in fixturedef.argnames}
         if inspect.isasyncgenfunction(func):
             agen = func(**params)
-            value = loop.run_until_complete(agen.__anext__())
+            value = LOOP.run_until_complete(agen.__anext__())
 
             def finalizer() -> None:
                 try:
-                    loop.run_until_complete(agen.__anext__())
+                    LOOP.run_until_complete(agen.__anext__())
                 except StopAsyncIteration:
                     pass
-                loop.close()
-                asyncio.set_event_loop(None)
 
             request.addfinalizer(finalizer)
             fixturedef.cached_result = (value, 0, None)
             return value
-        result = loop.run_until_complete(func(**params))
-        loop.close()
-        asyncio.set_event_loop(None)
+        result = LOOP.run_until_complete(func(**params))
         fixturedef.cached_result = (result, 0, None)
         return result
       
