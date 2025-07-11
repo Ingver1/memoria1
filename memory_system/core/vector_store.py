3b0842a4612e8791a10d6654ce678d432189184d
Evgeny <rage19671999@gmail.com>
2025-07-11 19:39:10 +0300

    Update vector_store.py

--- memory_system/core/vector_store.py
+++ memory_system/core/vector_store.py
@@ -293,7 +293,7 @@ class VectorStore:
         await self.flush()
 
     async def replicate(self) -> None:
-        self.flush()
+        await self.flush()
         ts = int(time.time())
         bak_path = self._bin_path.with_suffix(f".{ts}.bak")
         shutil.copy2(self._bin_path, bak_path)
